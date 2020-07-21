abda73d2a3df34621e1b0c8372151e6d421b7e75
Peter <pede.13@live.dk>
2020-07-21 23:33:56 +0200

    smallfix3

--- src/sorting/Main.java
+++ src/sorting/Main.java
@@ -5,19 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
         Printer printer = new Printer();
-        int[] intarray = {1,4,6,9,24,5,3,7,9,51,78,63,23,99,93,31,35,73};
+        int[] intArray = {1,4,6,9,24,5,3,7,9,51,78,63,23,99,93,31,35,73};
 
-        printer.printer(intarray);
+        printer.printer(intArray);
 
         MergeSort merger = new MergeSort();
         BubbleSort bubble = new BubbleSort();
         SelectionSort selection = new SelectionSort();
         InsertionSort insertion = new InsertionSort();
 
-        int[] mergedArray = merger.mergeSort(intarray);
-        int[] bubbleArray = bubble.bubbleSort(intarray);
-        int[] selectionArray = selection.selectionSort(intarray);
-        int[] insertionArray = insertion.insertionSort(intarray);
+        int[] mergedArray = merger.mergeSort(intArray);
+        int[] bubbleArray = bubble.bubbleSort(intArray);
+        int[] selectionArray = selection.selectionSort(intArray);
+        int[] insertionArray = insertion.insertionSort(intArray);
 
         printer.printer(mergedArray);
         printer.printer(bubbleArray);
